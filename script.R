761d9c94409f3f8d873fd4590a4d35012fc5ad5f
chiaramoruzzi <chiaramoruzzi@gmail.com>
2023-06-06 17:49:01 +0200

    prova factor analysis

--- script.R
+++ script.R
@@ -20,7 +20,7 @@ descr::freq(data1$v31)#
 descr::freq(data1$v33)#
 descr::freq(data1$v50)##
 descr::freq(data1$v32)#
-descr::freq(data1$v66)##
+#descr::freq(data1$v66)##
 #2
 descr::freq(data1$v22)#
 descr::freq(data1$v23)#
@@ -47,12 +47,13 @@ data1 <- data1 %>%
                        to=c(NA,3,5,4,3,2,1)))%>%
   mutate(v50=mapvalues(v50, from=c(-9,-8,1,2,3,4),
                        to=c(NA,NA,4,3,2,1)))%>%
-  mutate(v66=mapvalues(v66, from=c(-9,-8,-1,1,2,3,4),
-                       to=c(NA,NA,NA,4,3,2,1)))%>%
   mutate(v34=mapvalues(v34, from=c(-9,-8,1,2,3,4,5),
                        to=c(NA,3,5,4,3,2,1)))%>%
   mutate(v32=mapvalues(v32, from=c(-9,-8,0,1,2,3,4,5,6,7,8,9,10),
                        to=c(NA,NA,1,1,2,2,3,3,3,4,4,5,5)))
+
+#mutate(v66=mapvalues(v66, from=c(-9,-8,-1,1,2,3,4),
+#                     to=c(NA,NA,NA,4,3,2,1)))%>%
 
 #correlation matrix
 cor(data1, use = "complete.obs")
@@ -69,18 +70,20 @@ data1 <- na.omit(data1)
 fa.parallel(data1, fa="fa")#2
 
 factanal(~data1$v30+data1$v31+data1$v50+data1$v32
-         +data1$v22+data1$v23+data1$v24, factors = 2, rotation = "promax")
+         +data1$v22+data1$v23+data1$v24, factors = 2, rotation = "varimax")
 
+factanal(~data1$v30+data1$v31+data1$v50+data1$v32
+         +data1$v22+data1$v23+data1$v24, factors = 2)
 
 
 #F1 from "unfairness beliefs" to "personal beliefs"
 
 
-setwd("C:/Users/chiar/Desktop/Università/DAPS&CO/SOCIAL AND POLITICAL ATTITUDES/project/spat")
+setwd("C:/Users/chiar/Desktop/Università/DAPS&CO/SOCIAL AND POLITICAL ATTITUDES/project/Economic-Inequality")
 data <-rio::import( "ZA7600_v3-0-0.dta")
 
 
-data1 <- data.frame(data$v30, data$v31, data$v33, data$v50, data$v32, data$v22, data$v23, data$v24, data$v28, data$v34, data$v21)
+data_pr <- data.frame(data$v30, data$v31, data$v33, data$v50, data$v32, data$v22, data$v23, data$v24, data$v28, data$v34, data$v21)
 colnames(data1) <- c("v30", "v31", "v33", "v50", "v32", "v22", "v23", "v24", "v28", "v34", "v21")
 
 
